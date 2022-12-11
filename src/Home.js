f3fa4070cba940bcf4133a17c9d17a51a9e7d994
Abdullah Fitri Che Wan <ftk2431@gmail.com>
2022-12-11 20:24:05 +0800

    First Test - form validation

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,53 @@
-import "./App.css";
 import { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function Home() {
   const [text, setText] = useState("");
   const [isReady, setIsReady] = useState(false);
 
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      switch (event.target.value) {
+        case "Ready!":
+          setText(event.target.value);
+          setIsReady(true);
+        default:
+          break;
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    if (event.target.value === "") setIsReady(false);
+    setText(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          hidden={!isReady}
-          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-          className="App-logo"
-          alt="logo"
-          style={{ padding: "10px" }}
-        />
-        <b>
-          Requirement: Try to show the hidden image and make it clickable that
-          goes to /pokedex when the input below is "Ready!" remember to hide the
-          red text away when "Ready!" is in the textbox.
-        </b>
-        <p>Are you ready to be a pokemon master?</p>
-        <input type="text" name="name" />
-        <span style={{ color: "red" }}>I am not ready yet!</span>
+        <Link to="/pokedex">
+          <img
+            hidden={!isReady}
+            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+            className="App-logo"
+            alt="logo"
+            style={{ padding: "10px" }}
+          />
+        </Link>
+        <p className="text-white">Are you ready to be a pokemon master?</p>
+        <div className="container">
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={text}
+            onKeyDown={handleKeyDown}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          <span hidden={isReady} style={{ color: "red" }}>
+            I am not ready yet!
+          </span>
+        </div>
       </header>
     </div>
   );
